4834816f822880244309714ad858048610ce44a4
Keaton M <84339093+iKeaton@users.noreply.github.com>
2022-12-12 13:31:22 -0500

    refactor(`ManageMembersModal`): Prototype design (WIP)

--- apps/web/components/modals/ManageMembersModal.tsx
+++ apps/web/components/modals/ManageMembersModal.tsx
@@ -6,7 +6,10 @@ import {
     Error,
     Text,
   } from "@splashsaver/ui";
-  import { useState } from "react";
+  import { useEffect, useState } from "react";
+  import Image from "next/image";
+  import { useSession } from "next-auth/react";
+
   
   type Props = {
     setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
@@ -20,7 +23,11 @@ import {
     const [loading, setLoading] = useState(false);
     const [success, setSuccess] = useState("");
     const [error, setError] = useState("");
-    
+
+    const { data: session } = useSession();
+  
+    useEffect(() => {}, [session]);
+   
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
       e.preventDefault();
 
@@ -36,6 +43,21 @@ import {
           <Text>
             Add, remove, or edit members of your workspace.
           </Text>
+          <div className="my-3 m-auto flex items-center">
+            <Image
+            className="border rounded-full"
+            src={session.user?.image!}
+            width={35}
+            height={35}
+            quality={99}
+            title="Profile picture"
+            alt="Profile Picture "
+            />
+            <Text className="pl-2">
+              {session.user?.name}
+            </Text>
+            <Button className="ml-2 text-red-500" type="submit">Remove</Button>
+        </div>
           <div className="flex items-center justify-between pt-5">
             <Button type="submit">Save</Button>
           </div>
